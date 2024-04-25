e6d999bcac6e5ad2370cc5a64968c6c1c85bba59
GlennWSo <sondellglenn@gmail.com>
2024-04-25 10:05:28 +0200

    map with passtrhough

--- advent2305.py
+++ advent2305.py
@@ -12,11 +12,73 @@ so its two values are 50 and 51. With this information, you know that seed numbe
 corresponds to soil number 50 and that seed number 99 corresponds to soil number 51.
 """
 
-def make_map(d: int, s: int, z: int):
-    return {i + s: i + d for i in range(z)}
+from typing import Sequence, Tuple
 
 
-map1 = make_map(50, 98, 2)
+class Line:
+    def __init__(self, dest, source, size):
+        self.dest = dest
+        self.source = source
+        self.size = size
 
-print(map1)
-print(map1[99])
+    @classmethod
+    def mk_lines(cls, seq: Sequence[Tuple[int, int, int]]):
+        return [cls(d, s, z) for (d, s, z) in seq]
+
+
+def lines2map(lines: Sequence[Line]):
+    map = {}
+    for line in lines:
+        for i in range(line.size):
+            map[line.source + i] = line.dest + i
+
+    return map
+
+
+class Map:
+    def __init__(self, lines: Sequence[Line]):
+        self.map = lines2map(lines)
+
+    @classmethod
+    def from_seq(cls, seq: Sequence[Tuple[int, int, int]]):
+        return cls(Line.mk_lines(seq))
+
+    def __getitem__(self, i):
+        try:
+            return self.map[i]
+        except KeyError:
+            return i
+
+    def __repr__(self):
+        return repr(self.map)
+
+
+seed2soil = [
+    (50, 98, 2),
+    (52, 50, 48),
+]
+
+map1 = Map.from_seq(seed2soil)
+
+seeds = [
+    0,
+    1,
+    48,
+    49,
+    50,
+    51,
+    96,
+    97,
+    98,
+    99,
+]
+
+for i in seeds:
+    print(f"{i}: {map1[i]}")
+
+assert map1[79] == 81
+assert map1[14] == 14
+assert map1[55] == 57
+assert map1[13] == 13
+
+print("all done!")
