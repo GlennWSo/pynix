ba87d734a171cb1bc3071d310d7da168967cb088
GlennWSo <sondellglenn@gmail.com>
2024-04-25 10:49:06 +0200

    feeling naive

--- advent2305.py
+++ advent2305.py
@@ -82,3 +82,70 @@ assert map1[55] == 57
 assert map1[13] == 13
 
 print("all done!")
+
+
+test_input = """
+seeds: 79 14 55 13
+
+seed-to-soil map:
+50 98 2
+52 50 48
+
+soil-to-fertilizer map:
+0 15 37
+37 52 2
+39 0 15
+
+fertilizer-to-water map:
+49 53 8
+0 11 42
+42 0 7
+57 7 4
+
+water-to-light map:
+88 18 7
+18 25 70
+
+light-to-temperature map:
+45 77 23
+81 45 19
+68 64 13
+
+temperature-to-humidity map:
+0 69 1
+1 0 69
+
+humidity-to-location map:
+60 56 37
+56 93 4
+""".strip()
+
+
+def get_row(txt: str):
+    return [int(num) for num in txt.split(" ")]
+
+
+def get_map_nums(seq: Sequence[str]):
+    nums = []
+    for i, txt in enumerate(seq):
+        if not txt.strip():
+            return i, nums
+        nums.append(get_row(txt.strip()))
+
+    return i, nums
+
+
+lines = test_input.splitlines()
+pos = 0
+seeds = get_row(lines[pos][7:])
+print(seeds)
+print(lines[2])
+
+pos = 3
+i, seed2soil = get_map_nums(lines[pos:])
+print(seed2soil)
+
+pos += i + 2
+print(lines[pos - 1])
+i, soil2fertilizer = get_map_nums(lines[pos:])
+print(Map.from_seq(soil2fertilizer))
